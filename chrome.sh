73b1da6f28881c2002ec63b80f21fa1a8ad1d2c5
Tim Perry <pimterry@gmail.com>
2024-05-17 18:36:27 +0200

    Update Chrome & Chromedriver setup
    Chrome needs updating simply because the key seems to have been updated,
    breaking the Apt setup, so rebuilding alone will handle that.
    
    Chromedriver URL and structure has changed, so that needs a larger redo
    but this seems to work - mostly based on the details from GitHub's
    script.

--- chrome.sh
+++ chrome.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Based on https://github.com/nektos/act-environments/blob/940bf53/images/linux/scripts/installers/google-chrome.sh
+# Based on https://github.com/nektos/act-environments/blob/cc63076/images/linux/scripts/installers/google-chrome.sh and
+# https://github.com/actions/runner-images/blob/845c5ee8/images/ubuntu/scripts/build/install-google-chrome.sh
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
@@ -15,24 +16,31 @@ if ! command -v google-chrome; then
     exit 1
 fi
 
-CHROME_VERSION=$(google-chrome --product-version)
-CHROME_VERSION=${CHROME_VERSION%.*}
+FULL_CHROME_VERSION=$(google-chrome --product-version)
+CHROME_VERSION=${FULL_CHROME_VERSION%.*}
+
+echo "Installed Chrome version is $CHROME_VERSION"
 
 # Determine latest release of chromedriver
-# Compatibility of Google Chrome and Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/downloads/version-selection
-LATEST_CHROMEDRIVER_VERSION=$(curl --fail "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+# Compatibility of Google Chrome and Chromedriver: https://developer.chrome.com/docs/chromedriver/downloads/version-selection
+CHROME_PLATFORM="linux64"
+CHROME_VERSIONS_URL="https://googlechromelabs.github.io/chrome-for-testing/latest-patch-versions-per-build-with-downloads.json"
+CHROME_VERSIONS_JSON=$(curl -fsSL "${CHROME_VERSIONS_URL}")
+
+CHROMEDRIVER_VERSION=$(echo "${CHROME_VERSIONS_JSON}" | jq -r '.builds["'"$CHROME_VERSION"'"].version')
+CHROMEDRIVER_URL=$(echo "${CHROME_VERSIONS_JSON}" | jq -r '.builds["'"$CHROME_VERSION"'"].downloads.chromedriver[] | select(.platform=="'"${CHROME_PLATFORM}"'").url')
 
 # Download and unpack latest release of chromedriver
-echo "Downloading chromedriver v$LATEST_CHROMEDRIVER_VERSION..."
-wget "https://chromedriver.storage.googleapis.com/$LATEST_CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
-unzip chromedriver_linux64.zip
-rm chromedriver_linux64.zip
+echo "Downloading chromedriver v$CHROMEDRIVER_VERSION..."
+wget "$CHROMEDRIVER_URL"
+unzip chromedriver-linux64.zip
+rm chromedriver-linux64.zip
 
-CHROMEDRIVER_DIR="/usr/local/share/chrome_driver"
+CHROMEDRIVER_DIR="/usr/local/share/chrome-driver"
 CHROMEDRIVER_BIN="$CHROMEDRIVER_DIR/chromedriver"
 
 mkdir -p $CHROMEDRIVER_DIR
-mv "chromedriver" $CHROMEDRIVER_BIN
+mv "chromedriver-linux64/chromedriver" $CHROMEDRIVER_BIN
 chmod +x $CHROMEDRIVER_BIN
 ln -s "$CHROMEDRIVER_BIN" /usr/bin/
 echo "CHROMEWEBDRIVER=$CHROMEDRIVER_DIR" | tee -a /etc/environment
